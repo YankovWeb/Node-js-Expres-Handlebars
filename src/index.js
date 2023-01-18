cadb61aa54eb44afbe2fdfb05d946debbb06b363
Grigor Yankov <yankovweb@outlook.com>
2023-01-18 19:48:06 +0200

    [SetConfig]

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-const express = require('express');
-
-const config = require('./config');
-
+const express = require("express");
+const handlebars = require("express-handlebars");
 const app = express();
+const config = require("./config/config");
 
-app.get('/', (req, res) => {
-    res.send('Home page');
+require("./config/viewEngine")(app);
+
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "./src/views");
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
+app.listen(config.PORT, () =>
+  console.log(`Server is running on port ${config.PORT}...`)
+);
