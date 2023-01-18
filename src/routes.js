1ad8f8f0b3ac98a639a9e349f0ca9f9a50fd3852
Grigor Yankov <yankovweb@outlook.com>
2023-01-18 21:19:03 +0200

    Cube

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,11 @@
 const router = require("express").Router();
+
 const cubeController = require("./controllers/cubController");
+const homeCotnroller = require("./controllers/homeController");
 
-router.get("/", (req, res) => {
-  res.render("index");
-});
-
-router.get("/about", (req, res) => {
-  res.render("about");
-});
-
-// app.get("/create", (req, res) => {
-//   res.render("create");
-// });
-router.get("./create", cubeController.getCreateCube);
+router.get("/", homeCotnroller.getHomePage);
+router.get("/about", homeCotnroller.getAboutPage);
+router.get("/create", cubeController.getCreateCube);
+router.post("/create", cubeController.postCreateCube);
 
 module.exports = router;
